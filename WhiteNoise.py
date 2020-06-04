215a53998d65518e5de7e6e275f7602be800c5f6
Greg Broiles <gbroiles@gmail.com>
2020-06-04 14:05:40 -0700

    small changes, randomize sleep value

--- WhiteNoise.py
+++ WhiteNoise.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env/python3
 import random
 import re
 import subprocess
@@ -6,7 +6,8 @@ import sys
 from time import sleep
 import requests
 from bs4 import BeautifulSoup
-# ALEXA_SOURCE is the webpage to pull links from 
+
+# ALEXA_SOURCE is the webpage to pull links from
 ALEXA_SOURCE = "https://www.alexa.com/topsites/category/Top/Science"
 
 # ALEXASKIP = list of strings/URL's to skip when assembling list of links
@@ -24,40 +25,40 @@ SITEINFO = "siteinfo"
 # TIMEOUT = URL fetch will stop trying after this many seconds
 TIMEOUT = 30
 
-site = ALEXA_SOURCE
 links = []
 
-print("Chose Random URL capture URL: {}".format(site))
+print("Chose Random URL capture URL: {}".format(ALEXA_SOURCE))
 
 try:
-    html = requests.get(site, timeout=TIMEOUT).text
+    html = requests.get(ALEXA_SOURCE, timeout=TIMEOUT).text
 except:
-    print("Can't fetch {}".format(site))
+    print("Can't fetch {}".format(ALEXA_SOURCE))
     sys.exit(1)
 
 try:
     soup = BeautifulSoup(html, features="html.parser")
 except:
-    print("Can't parse HTML from {}".format(site))
-    sys.exit(1)
+    print("Can't parse HTML from {}".format(ALEXA_SOURCE))
 
-for line in soup.find_all("a"): #find all anchor tags
-    link = line.get("href") #find href's
+for line in soup.find_all("a"):  # find all anchor tags
+    link = line.get("href")  # find href's
     if re.search(SITEINFO, str(link)) is not None and link not in ALEXASKIP:
-        links.append(link) #build list of link candidates
+        links.append(link)  # build list of link candidates
 
-linknum = len(links)
-
-if linknum > 2: # if < 2, something has gone wrong, don't try to launch browser
-    target = random.randint(0, linknum - 1)
+if len(links) > 2:  # if < 2, something has gone wrong, don't try to launch browser
+    target = random.randint(0, len(links) - 1)
     print("Chose Random Link no {}".format(target))
-    final = re.sub("/siteinfo/", "http://www.", links[target]) #rewrite link
-    print("Launching URL and closing {} seconds later...".format(DELAY))
-    browser = subprocess.Popen(
+    final = re.sub("/siteinfo/", "http://www.", links[target])  # rewrite link
+    random.seed()  # initialize RNG
+    delay = (
+        DELAY * 2 * random.random()
+    )  # wait random # of seconds approx equal to DELAY
+    print("Launching URL and closing {} seconds later...".format(delay))
+    browser = subprocess.Popen(  # launch browser
         [BROWSERNAME, final],
-        stdout=subprocess.DEVNULL, #don't clutter screen w/ output
-        stderr=subprocess.DEVNULL, #don't clutter screen w/ errors
+        stdout=subprocess.DEVNULL,  # don't clutter screen w/ output
+        stderr=subprocess.DEVNULL,  # don't clutter screen w/ errors
     )
-    sleep((DELAY) #wait while we load/pretend to read the page
+    sleep(delay)
     print("Kill the process")
     browser.terminate()
